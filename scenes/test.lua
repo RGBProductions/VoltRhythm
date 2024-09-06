28b8293da60d9c80d6cffdc321c8dee6ca1d506f
RGBProductions <luvm1n3craft@gmail.com>
2024-09-05 18:26:59 -0700

    6 default song covers

--- scenes/test.lua
+++ scenes/test.lua
@@ -1,9 +1,25 @@
 local scene = {}
 
-local img = love.graphics.newImage("menutest.png")
+local images = {
+    [0] = love.graphics.newImage("images/default0.png"),
+    [1] = love.graphics.newImage("images/default1.png"),
+    [2] = love.graphics.newImage("images/default2.png"),
+    [3] = love.graphics.newImage("images/default3.png"),
+    [4] = love.graphics.newImage("images/default4.png"),
+    [5] = love.graphics.newImage("images/default5.png"),
+    [6] = love.graphics.newImage("test_cover.png")
+}
+
+local i = 0
+
+function scene.keypressed(k)
+    if k == "space" then
+        i = (i + 1) % 8
+    end
+end
 
 function scene.draw()
-    love.graphics.draw(img)
+    love.graphics.draw(images[i], 128, 128, 0, 2, 2)
 end
 
 return scene
