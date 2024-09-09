5e74ba05f8cc6fe81dda8fa30929cabd497cd697
FuryDev <33258743+RGBProductions@users.noreply.github.com>
2024-09-09 16:18:14 -0700

    Fix version retrieval on non-unix systems

--- version.lua
+++ version.lua
@@ -1,7 +1,7 @@
 return function()
     local data = love.filesystem.read("version")
     while data:find("\r") do
-        data:gsub("\r","")
+        data = data:gsub("\r","")
     end
     local lines = data:split("\n")
     return {name = lines[1], version = lines[2], code = lines[3]}
