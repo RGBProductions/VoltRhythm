fb8f07cfd690b0b72129e2e7c5e8a6434ac2b49f
RGBProductions <luvm1n3craft@gmail.com>
2025-10-23 18:37:48 -0700

    thats not what i meant

--- scenes/setup.lua
+++ scenes/setup.lua
@@ -164,7 +164,7 @@ function scene.gamepadpressed(s, b)
     end
     if b == "x" then
         if scene.state == 0 then
-            scene.textinput("space")
+            scene.textinput(" ")
         end
     end
     if b == "start" then
